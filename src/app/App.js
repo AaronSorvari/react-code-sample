fd0d555db52eba1cc8d190e4e20c840df4658af7
Aaron Sorvari <aaron.sorvari@ilmservice.com>
2016-12-12 11:41:37 -0600

    apply "react-web" yeoman generator

---
+++ src/app/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { Provider } from 'react-redux';
+import { AppContainer } from 'react-hot-loader';
+
+import configureStore from './store/configureStore';
+import routes from './routes';
+import AppActions from './actions/AppActions';
+
+let store;
+let history;
+
+const App = {
+    /**
+     * Run application
+     */
+    run() {
+        // create store
+        store = configureStore();
+
+        // create browser history for router
+        history = syncHistoryWithStore(browserHistory, store);
+
+        // render aplication
+        ReactDOM.render(
+            <AppContainer key={Math.random()}>
+                <Provider store={store}>
+                    <Router history={history}>{routes}</Router>
+                </Provider>
+            </AppContainer>,
+            document.getElementById('container')
+        );
+
+        // dispatch initialize action
+        store.dispatch(AppActions.initialize());
+    }
+};
+
+
+if (module.hot) {
+    module.hot.accept('./routes', () => {
+        const nextRoutes = require('./routes').default;
+
+        ReactDOM.render(
+            <AppContainer key={Math.random()}>
+                <Provider store={store}>
+                    <Router history={history}>{nextRoutes}</Router>
+                </Provider>
+            </AppContainer>,
+            document.getElementById('container')
+        );
+    });
+}
+
+export default App;
---
+++ src/app/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router';
+
+import Template from './containers/Template/Template';
+
+/**
+ * Define routes
+ */
+const routes = (
+    <Route path="/" component={Template}>
+        <Redirect from="*" to="/" />
+    </Route>
+);
+
+export default routes;
