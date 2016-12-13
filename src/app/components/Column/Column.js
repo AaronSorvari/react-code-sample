22cd6073bcffc5441bb1235da1ae01927e6ee380
Aaron Sorvari <aaron.sorvari@ilmservice.com>
2016-12-13 15:34:36 -0600

    button for deleting a column

--- src/app/components/Column/Column.js
+++ src/app/components/Column/Column.js
@@ -3,8 +3,11 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import Paper from 'material-ui/Paper';
 import { Toolbar, ToolbarGroup, ToolbarTitle } from 'material-ui/Toolbar';
+import IconMenu from 'material-ui/IconMenu';
 import IconButton from 'material-ui/IconButton';
-import ContentAdd from 'material-ui/svg-icons/content/add';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import MenuItem from 'material-ui/MenuItem';
+import Divider from 'material-ui/Divider';
 import AppActions from '../../actions/AppActions';
 import selectors from '../../selectors/entitySelectors';
 import style from './column.scss';
@@ -21,13 +24,28 @@ class Column extends React.Component {
     constructor(props, context) {
         super(props, context);
 
-        this.handleAddButtonClicked = this.handleAddButtonClicked.bind(this);
+        this.handleAddCardClicked = this.handleAddCardClicked.bind(this);
+        this.handleDeleteColumnClicked = this.handleDeleteColumnClicked.bind(this);
     }
 
-    handleAddButtonClicked() {
+    handleAddCardClicked() {
         this.props.actions.showCardEntryModal({
             boardId: this.props.boardId,
             columnId: this.props.columnId
+        });
+    }
+
+    handleDeleteColumnClicked() {
+        const boardId = this.props.boardId;
+        const columnId = this.props.columnId;
+        const columnName = this.props.column.label;
+
+        this.props.actions.showConfirmModal({
+            title: 'Confirm',
+            content: `Are you sure you want to delete the column "${columnName}"?`,
+            confirmAction: () => {
+                this.props.actions.deleteColumnAsync({ boardId, columnId });
+            }
         });
     }
 
@@ -45,9 +63,11 @@ class Column extends React.Component {
                         <ToolbarTitle text={this.props.column.label} />
                     </ToolbarGroup>
                     <ToolbarGroup lastChild>
-                        <IconButton onClick={this.handleAddButtonClicked}>
-                            <ContentAdd />
-                        </IconButton>
+                        <IconMenu iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}>
+                            <MenuItem primaryText="Add Card" onClick={this.handleAddCardClicked} />
+                            <Divider />
+                            <MenuItem primaryText="Delete Column" onClick={this.handleDeleteColumnClicked} />
+                        </IconMenu>
                     </ToolbarGroup>
                 </Toolbar>
                 <div className={style.content}>
